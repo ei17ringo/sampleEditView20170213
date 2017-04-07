38617a47425d83ab112bb97c39b01ab723c98685
ei17ringo <ei17ringo@gmail.com>
2017-04-07 16:04:05 +0800

    commit init

--- sampleEditView/ViewController.swift
+++ sampleEditView/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var myTitle: UITextField!
+    
+    @IBOutlet weak var myContents: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapClose(_ sender: UIButton) {
+    
+        myContents.resignFirstResponder()
+    
+    }
+    
+    //タイトルのキーボードでリターンが押された時
+    @IBAction func tapReturn(_ sender: UITextField) {
+    }
+    
+    //テキストフィールド入力開始
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        print("textFieldShouldBeginEditing")
+        
+        print(textField.tag)
+        
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
